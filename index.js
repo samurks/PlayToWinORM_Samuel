8f3f38b5a9ad3d821fe6b55ac17eea543c090a78
samurks <samueltorochagas@gmail.com>
2024-04-19 14:46:30 -0400

    Creation of the Usuario model and execution of the first sync

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 require("dotenv").config();
 const conn = require("./db/conn")
 
-conn.authenticate().then( () => {
-    console.log("Conectado ao banco de dados com sucesso!");
+const Usuario = require("./models/Usuario")
+
+conn
+.sync()
+.then( () => {
+    console.log("Conectado e sincronizado com o banco de dados!");
 }).catch( (err)=>{
     console.log("Ocorreu um erro: " + err)
 } );
+
+
+
+//conn.authenticate()
+//.then( () => {
+//    console.log("Conectado ao banco de dados com sucesso!");
+//}).catch( (err)=>{
+//    console.log("Ocorreu um erro: " + err)
+//} );
---
+++ models/Usuario.js
@@ -0,0 +1,17 @@
+const conn = require("./db/conn")
+const { DataTypes } = require("sequelize")
+
+const Usuario = db.define("Usuario",{
+nickname: {
+    type:DataTypes.STRING,
+    required: true,  
+},
+nome: {
+    type:DataTypes.STRING,
+    required: true,  
+},
+    
+});
+
+
+module.exports = Usuario;
