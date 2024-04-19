1d6c878d3f4351af6e1a96206819704ce8a52f34
samurks <samueltorochagas@gmail.com>
2024-04-19 15:36:17 -0400

    create Jogo model

--- index.js
+++ index.js
@@ -1,7 +1,35 @@
 require("dotenv").config();
-const conn = require("./db/conn")
+const conn = require("./db/conn");
 
-const Usuario = require("./models/Usuario")
+const Usuario = require("./models/Usuario");
+
+const express = require("express");
+const app = express();
+
+app.use(
+    express.urlencoded({
+        extended: true,
+    })
+);
+
+app.use(express.json());
+
+app.get("/usuarios/novo", (req, res)=>{
+res.sendFile(`${__dirname}/views/formusuario.html`);
+});
+
+app.post("/usuarios/novo", async (req, res)=>{
+const dadosUsuario = {
+    nickname: req.body.nickname,
+    };
+
+    const usuario = await Usuario.create(dadosUsuario)
+    res.send("Usuario Inserido sob o id" + usuario.id)
+});
+
+app.listen(8000, ()=>{
+    console.log("Server Rodando")
+})
 
 conn
 .sync()
